e36b913c77415a3ca722906e6b6e3e1c906a470e
Viveksanthosh <viveksanthosh@holidayiq.com>
2018-08-16 23:49:18 +0530

    made  todo app

--- src/App.js
+++ src/App.js
@@ -3,32 +3,60 @@ import './App.css';
 import { observable, autorun, computed } from 'mobx';
 import { observer } from 'mobx-react';
 
-class Counter {
-    @observable count = 0
-    @computed get total(){
-        console.log('comouting total');
-        return this.count * 3;
+class Todo {
+    @observable value = ''
+    @observable checked
+    constructor(value) {
+        this.value = value;
+        this.checked = false;
     }
 }
 
-let counter = new Counter();
+class Todos {
+    @observable list = [new Todo('sleep')]
+    @observable filter = ''
+    @computed get filteredTodos (){
+        let reg = new RegExp(this.filter, 'i');
+        return this.list.filter(t => !this.filter || reg.test(t.value))
+    }
+}
+
+
+let todos = new Todos();
 
 @observer class App extends React.Component {
+
+    handleNewTodo = e => {
+        if (e.key === 'Enter') {
+            todos.list.push(new Todo(e.target.value));
+            e.target.value = '';
+        }
+    }
+
+    handleFilterChange = e => {
+        todos.filter = e.target.value
+    }
+
+    toggleCheckBox = (todo) => {
+        todo.checked = !todo.checked;
+    }
+
     render() {
         return (
             <React.Fragment>
-                <button onClick={() => { counter.count++ }}>increment</button>
-                <button onClick={() => { counter.count-- }}>decrement</button>
-                <br />
-                <p><b>Count </b>{counter.count}</p>
-                <p><b>Total </b>{counter.total}</p>
+                <label>new</label><input onKeyPress={this.handleNewTodo} />
+                <label>filter</label><input onChange = {this.handleFilterChange} />
+                <ul>
+                    {todos.filteredTodos.map(todo => <li>
+                        <input type="checkbox" onChange={e => this.toggleCheckBox(todo)}  checked={todo.checked} />{todo.value}</li>)}
+                </ul>
             </React.Fragment>
         );
     }
 }
 
 if (typeof window !== 'undefined') {
-    window.counter = counter;
+    window.counter = todos;
 }
 
 autorun(() => {
