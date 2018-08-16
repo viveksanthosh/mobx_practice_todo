87cd3035d5172edb652b3552661b148d165bad0f
Viveksanthosh <viveksanthosh@holidayiq.com>
2018-08-16 22:40:56 +0530

    moved observer out of react componet

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import { observable, autorun } from 'mobx';
 import { observer } from 'mobx-react';
 
-@observer class App extends React.Component {
+class Counter{
     @observable count = 0
+}
+
+let counter = new Counter();
+
+@observer class App extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <button onClick={() => { this.count = this.count + 1 }}>increment</button>
-                <button onClick={() => { this.count = this.count - 1 }}>decrement</button>
+                <button onClick={() => { counter.count = counter.count + 1 }}>increment</button>
+                <button onClick={() => { counter.count = counter.count - 1 }}>decrement</button>
                 <br />
-                <p>{this.count}</p>
+                <p>{counter.count}</p>
             </React.Fragment>
         );
     }
